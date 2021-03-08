66a3e4e5b460476d4e98886fe9146905466786b5
JPval18057 <78621543+JPval18057@users.noreply.github.com>
2021-03-08 17:10:50 -0600

    Update ESP_final.ino
    le agregue comentarios

--- ESP_final/ESP_final.ino
+++ ESP_final/ESP_final.ino
@@ -7,7 +7,7 @@
  * AUTOR : JUAN PABLO VALENZUELA
  * 
  */
-
+//Algunas líneas de código son originales porque este código se basó en el ejemplo de arduino
 /************************** Configuration ***********************************/
 
 // edit the config.h tab and enter your Adafruit IO credentials
@@ -17,21 +17,21 @@
 
 /************************ Example Starts Here *******************************/
 
-// this int will hold the current count for our sketch
+//estas son las variables de trabajo
 int count = 0;
 int TEMP_LSB = 0; // for incoming serial data
 String led_0="a";
 char envio = 0;
 
 
-// set up the 'counter' feed
+// configuramos los feeds
 AdafruitIO_Feed *counter = io.feed("LED");
 AdafruitIO_Feed *temp = io.feed("TEMP");
 AdafruitIO_Feed *LED0 = io.feed("LED0");
 AdafruitIO_Feed *LED1 = io.feed("LED1");
 AdafruitIO_Feed *LED2 = io.feed("LED2");
 
-//MESSAGE HANDLER
+//MESSAGE HANDLER, interrupciones del dashboard
 void handleMessage(AdafruitIO_Data *data) {
 
   //Serial.print("received <- Pushbutton ");
@@ -42,28 +42,27 @@ void handleMessage(AdafruitIO_Data *data) {
 
 void setup() {
 
-  // start the serial connection
+  // iniciamos la interfaz serial
   Serial.begin(9600);
 
-  // wait for serial monitor to open
+  // esperamos a que se ponga lista la comunicación
   while(! Serial);
 
-  //Serial.print("Connecting to Adafruit IO");
+  
 
-  // connect to io.adafruit.com
+  // conectamos a io.adafruit.com
   io.connect();
 
-  // wait for a connection
+  // esperamos a que se conecte
   while(io.status() < AIO_CONNECTED) {
     //Serial.print(".");
     delay(500);
   }
 
-  // we are connected
-  //Serial.println();
-  //Serial.println(io.statusText());
+  //Acá se ya está conectado y solo configuramos el led integrado para verificar visualmente
   pinMode(2, OUTPUT);
 
+  //configuramos las salidas de los mensajes que entran en el dashboard
   LED0->onMessage(handleMessage);  
   LED0->get();
   LED1->onMessage(handleMessage);
@@ -74,10 +73,7 @@ void setup() {
 
 void loop() {
 
-  // io.run(); is required for all sketches.
-  // it should always be present at the top of your loop
-  // function. it keeps the client connected to
-  // io.adafruit.com, and processes any incoming data.
+  //iniciamos la conexión al servidor para enviar datos
   io.run();
 
   if (Serial.available() > 0) {
@@ -85,31 +81,27 @@ void loop() {
     TEMP_LSB = Serial.read();
     TEMP_LSB = TEMP_LSB/4;//Procesamos el dato
 
-    // say what you got:
-    //Serial.print("I received: ");
-    //Serial.println(TEMP_LSB, HEX);
+    //guardamos los datos que entran por comunicación serial, las interrupciones son automáticas
   }
   
-  // save count to the 'counter' feed on Adafruit IO
-  //Serial.print("sending -> ");
-  //Serial.println(count);
+  // enviamos el valor del contador al feed counter
   counter->save(count);
   
   
-  // increment the count by 1
+  // aumentamos el contador
   count++;
-  if (count==151){ //RESETING THE COUNTER
+  if (count==151){ //se reinicia el contador
     count=0;
   }
-  // Adafruit IO is rate limited for publishing, so a delay is required in
-  // between feed->save events. In this example, we will wait three seconds
-  // (1000 milliseconds == 1 second) during each loop.
+  // Adafruit IO no puede recibir datos tan rápido y es necesario ponerle un delay de por lo menos 2 segundos
+  // entre cada lectura/escritura
   digitalWrite(2,LOW);
   delay(6000);
   temp->save(TEMP_LSB);
   digitalWrite(2, HIGH);
   delay(6000);
 
+  //seleccionamos que caracter vamos a enviar para que el pic encienda o apague una led
   if (led_0=="ON0"){
     envio = 0x0A;
   }
@@ -128,6 +120,8 @@ void loop() {
   if (led_0=="OFF2"){
     envio = 0xFC;
   }
+  
+  //Enviamos el dato al PIC por eusart
   //Serial.println(led_0);
   //Serial.println(envio, HEX);
   Serial.write(envio);
